5ca3b7f40a037a1ecc7f3dc1214e7645728732eb
Apocryphon06 <hrithikchandrashekar@gmail.com>
2022-11-18 10:07:51 +0530

    clean up

--- src/index.js
+++ src/index.js
@@ -4,23 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
-import allReducers from "./reducers";
-import { Provider } from "react-redux";
 import { PathContextProvider } from "./context/PathContext";
-
-let store = createStore(allReducers);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <PathContextProvider>
-    <Provider store={store}>
-      <BrowserRouter>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
   </PathContextProvider>
 );
 
